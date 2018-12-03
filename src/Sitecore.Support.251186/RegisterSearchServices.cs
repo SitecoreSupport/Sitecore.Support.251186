a07b0b8464b75cb12cbdd0d9759b13384268b2d9
iro <iro@sitecore.net>
2018-12-03 13:17:37 +0200

    Initial to make compatible

---
+++ src/Sitecore.Support.251186/RegisterSearchServices.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.XA.Foundation.Search.Pipelines.IoC
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.DependencyInjection;
+  using Sitecore.XA.Foundation.Search.Interfaces;
+  using Sitecore.XA.Foundation.Search.Services;
+  using Sitecore.XA.Foundation.Search.Wrappers;
+
+  public class RegisterSearchServices : IServicesConfigurator
+  {
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddSingleton<IFacetService, Sitecore.Support.XA.Foundation.Search.Services.FacetService>();
+    }
+  }
+}
